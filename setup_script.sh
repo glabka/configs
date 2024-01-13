8e9cf8cbbca35330ae9e349e0bd2afeb7d1373de
glabka glabka <glabka333@gmail.com>
2024-01-13 09:07:22 +0100

    Readme.md and some config files for Ubuntu, vim and tmux added plus scirpt for shortcut change for gnome-terminal.

---
+++ setup_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# gnome-terminal shortcuts
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ next-tab '<Control>Tab'
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ prev-tab '<Control><Shift>Tab'
+
